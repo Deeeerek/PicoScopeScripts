32b8ee30128a0878ef0d2c64a0258e63c7106b05
Mateusz Kowalik <msz.kowalik@gmail.com>
2018-02-03 17:48:46 +0100

    Angle Calculation works now, next is adding option to save file. final step is using Qt in other file to use qui

--- CircuitResponse.py
+++ CircuitResponse.py
@@ -30,18 +30,41 @@ from picoscope import ps2000a
 import pylab as plt
 import numpy as np
 import peakutils
+import scipy
+from scipy.signal import correlate
 
-def avgDelay(input,output,minDist):
-    inputPeaks = peakutils.indexes(input, thres=0.5, min_dist=minDist)
-    outputPeaks = peakutils.indexes(output, thres=0.5, min_dist=minDist)
+ranges = [2.0 , 1.0 , 0.5 , 0.2 , 0.1 , 0.05]
+samplesPerObservation = 16384
+currentRange = ranges[0]
+nSamples = 0;
 
-    avgDifference = 0;
-    for i in range(inputPeaks.size):
-        if i < outputPeaks.size:
-            avgDifference +=  outputPeaks[i] - inputPeaks[i]
+def getSuitableRange(amp):
+    inputmeasurementRange = ranges[0]; #assign highest possible range 
+    for id,range in enumerate(ranges): # go trough all possible ranges
+        if amp  < range : #check if value id within range 
+            inputmeasurementRange = range
+    return inputmeasurementRange
+def getChannels(ps):
+    ps.runBlock()
+    ps.waitReady()
 
-    return avgDifference / min([inputPeaks.size,outputPeaks.size])
-def AmpPhase(ps, freq, genAmp,lastRange):
+    ps.runBlock()
+    ps.waitReady()
+    input = ps.getDataV('A', nSamples, returnOverflow=False)
+    output = ps.getDataV('B', nSamples, returnOverflow=False)
+    return (input,output)
+def measureChannelsAutoRange(ps):
+    (input,output) = getChannels(ps)
+    global currentRange
+    if currentRange != getSuitableRange(max(output)):
+        currentRange = getSuitableRange(max(output))
+        ps.setChannel('B', 'DC', currentRange, 0.0, enabled=True, BWLimited=False)
+        (input,output) = getChannels(ps)
+
+    return (input,output)
+
+
+def AmpPhase(ps, freq, genAmp):
     Amp = 0.0
     Phase = 0.0
 
@@ -50,70 +73,65 @@ def AmpPhase(ps, freq, genAmp,lastRange):
     observationPeriod = (1/freq)*periods
     period = 1/freq
     outputRange = 2.0
-    ranges = [2.0 , 1.0 , 0.5 , 0.2 , 0.1 , 0.05]
-    samplesPerObservation = 16384
+   
 
-    for idx, range in enumerate(ranges):
-       
-       
         
-        if (range > lastRange):
-            continue
-        else:
-            lastRange = range
+    sampling_interval = observationPeriod / samplesPerObservation
+    (actualSamplingInterval, nSamples, maxSamples) = ps.setSamplingInterval(sampling_interval, observationPeriod)
+    # enable AWG
+    ps.setSigGenBuiltInSimple(offsetVoltage=0, pkToPk=genAmp, waveType="Sine", frequency=freq)
+    
+    #channel A is always connected to Gen, so no ampitude is 
+    ps.setChannel('A', 'DC', getSuitableRange(genAmp), 0.0, enabled=True, BWLimited=False)
+    ps.setChannel('B', 'DC', getSuitableRange(currentRange), 0.0, enabled=True, BWLimited=False)
 
-        sampling_interval = observationPeriod / samplesPerObservation
-        (actualSamplingInterval, nSamples, maxSamples) = ps.setSamplingInterval(sampling_interval, observationPeriod)
+    ps.setSimpleTrigger('A', 0.0, 'Rising', timeout_ms=100, enabled=True)
 
-        # the setChannel command will chose the next largest amplitude
-        channelRange = ps.setChannel('A', 'DC', range, 0.0, enabled=True, BWLimited=False)
-        ps.setChannel('B', 'DC', range, 0.0, enabled=True, BWLimited=False)
+    (input,output) =  measureChannelsAutoRange(ps)
+    
+    dataTimeAxis = np.arange(nSamples)
+    Amp = max(output)
 
-        ps.setSimpleTrigger('A', 0.0, 'Rising', timeout_ms=100, enabled=True)
 
-        ps.setSigGenBuiltInSimple(offsetVoltage=0, pkToPk=genAmp, waveType="Sine", frequency=freq)
 
-        ps.runBlock()
-        ps.waitReady()
-        time.sleep(0.5)
-        ps.runBlock()
-        ps.waitReady()
-        input = ps.getDataV('A', nSamples, returnOverflow=False)
-        output = ps.getDataV('B', nSamples, returnOverflow=False)
+    # in samples
 
-        dataTimeAxis = np.arange(nSamples)
-        Amp = max(output)
+    y_rad=np.arccos(np.dot(input,output)/(np.linalg.norm(input)*np.linalg.norm(output)))
+    y_deg=y_rad*360/(2*np.pi)
 
-        # in samples
-        timeDelay = (avgDelay(input, output, samplesPerObservation/periods*4)*(actualSamplingInterval) )
-        Phase =  timeDelay/period * 360
-        if max(output) > range/2 :
-            break
+
+    # force the phase shift to be in [-pi:pi]
+    recovered_phase_shift = y_deg
+
 
 
     print("Frequency: " + str(freq))
     print("Amplitude: " + str(Amp))
-    print("Phase: " + str(Phase))
-    return (Phase,Amp*2,lastRange) # return phase in degrees and P-P 
+    print("Phase: " + str(recovered_phase_shift))
+    return (recovered_phase_shift,Amp) # return phase in degrees and P-P 
 
 
 if __name__ == "__main__":
     print(__doc__)
-
+    
     print("Attempting to open Picoscope 2000...")
 
    # ps = ps2000.PS2000()
     ps = ps2000a.PS2000a()  # Uncomment this line to use with the 2000a/2000b series
 
-    print("Found the following picoscope:")
+    print("Found the following picoscope:") 
     print(ps.getAllUnitInfo())
+    FStart = 100
+    FEnd  = 20000
+    FStep = 100
 
-    frequencies = range(10000,1000000,10000)
+
+    frequencies = range(FStart,FEnd,FStep)
     Phases = []
     Amplitudes = []
     lastRange = 2.0
     for freq in frequencies:
-        (Phase,Amp,lastRange) = AmpPhase(ps,freq,1.2,lastRange)
+        (Phase,Amp) = AmpPhase(ps,freq,3.0)
         Phases.append(Phase)
         Amplitudes.append(Amp)
 
@@ -140,4 +158,3 @@ if __name__ == "__main__":
     plt.xlabel("Frequency")
     plt.legend()
     plt.show()
-    
